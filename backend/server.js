7d6b3cad1f42ccf00ea60537250778769cf6c4a1
NataGian <nataliegiannouli@gmail.com>
2022-06-03 18:46:20 +0300

    headers

--- backend/server.js
+++ backend/server.js
@@ -10,6 +10,12 @@ connectDB()
 
 const app = express()
 
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+    res.header("Access-Control-Allow-Headers", "x-access-token, Origin, X-Requested-With, Content-Type, Accept");
+    next();
+  });
 app.use(express.json())
 app.use(express.urlencoded({extended: false}))
 
